7d36376ae509cbee871d30a7c1f375e7132c761e
dev1_clofast <gnani4412@gmail.com>
2024-08-17 18:01:57 +0530

    added global footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,6 @@ const Footer = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        mb: "0.8rem",
       }}
     >
       <Typography color={main}>
